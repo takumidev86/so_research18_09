eb4ed09c6b3a9bbb0ea57311cf88d6486a97ffdc
takumidev86 <takumidevelopment86@gmail.com>
2021-01-08 17:11:49 +0900

    [Id_extraction]

--- SotorrentMining/csv/Id_extraction.py
+++ SotorrentMining/csv/Id_extraction.py
@@ -1,29 +1,24 @@
 import csv
-import pprint
 from properties import edits_test_path
-import pandas 
 
-
-
+def search_postId(postId_count,postId_list):
+    print(postId_count)
+    print(postId_list)
 
 def number_split():
-    number_count = 0
-    ans = []
+    postId_count = 0
+    postId_list = []
     with open(edits_test_path) as f:
         for row in csv.reader(f):
             tab_split= f"{row}".split('\\t')
             for i in tab_split:
                 if i.isnumeric():
-                    output = int(i)
-                    print(output)
-                    number_count = number_count + 1
-                    ans.append(output)
-        print(ans)
-        print(number_count)
+                    postId_count = postId_count + 1
+                    postId_list.append(postId)
+    search_postId(postId_count,postId_list)
 
 def main():
     number_split()
-    
 
 if __name__ == '__main__':
     main()
